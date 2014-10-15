79feee821a28497a5b7e656fbe8d9ca5aadc9d65
Trip Volpe <trip@flowroute.com>
2014-10-15 01:41:53 -0700

    Make the piggy bounce!

--- main.cpp
+++ main.cpp
@@ -1,11 +1,70 @@
 #include <iostream>
+#include <cmath>
 
 #include "sdl.h"
+#include "vec2.h"
 #include "scopeguard.h"
 
 const int SCREEN_W = 920;
 const int SCREEN_H = 480;
 const int TILE_SIZE = 256;
+const double PI = 3.14159265;
+
+typedef sdl::renderer::texture texture;
+
+/*
+ * Function: render_scene
+ *
+ * Render a tiled background with a sprite at a given location.
+ */
+void render_scene(sdl::renderer& renderer,
+        texture const& bg, texture const& sprite, int x, int y)
+{
+    int xtiles = SCREEN_W / TILE_SIZE + 1;
+    int ytiles = SCREEN_H / TILE_SIZE + 1;
+
+    for (int ty = 0; ty < ytiles; ++ty) {
+        for (int tx = 0; tx < xtiles; ++tx) {
+            renderer.copy(bg, tx*TILE_SIZE, ty*TILE_SIZE, TILE_SIZE, TILE_SIZE);
+        }
+    }
+
+    renderer.copy(sprite, x, y);
+}
+
+
+/*
+ * Function: random
+ *
+ * Return a normally-distributed random double between given bounds.
+ */
+double random(double from = 0.0, double to = 1.0)
+{
+    return from + to * ((double)rand() / (double)RAND_MAX);
+}
+
+
+/*
+* Function: normal
+*
+* Find nearest edge or corner normal according to which edges
+* of a bounding box have been exceeded (from within).
+*/
+vec2 normal(bool left, bool top, bool right, bool bottom) {
+    double N;
+    if      (top && left)       { N = -PI/4;    }
+    else if (top && right)      { N = -3*PI/4;  }
+    else if (bottom && left)    { N = PI/4;     }
+    else if (bottom && right)   { N = 3*PI/4;   }
+    else if (top)               { N = -PI/2;    }
+    else if (bottom)            { N = PI/2;     }
+    else if (left)              { N = 0;        }
+    else if (right)             { N = PI;       }
+    else {
+        throw std::runtime_error("nonsense collision");
+    }
+    return vec2(N);
+}
 
 
 int main(int argc, char** argv)
@@ -26,22 +85,46 @@ int main(int argc, char** argv)
     // Load textures
     auto bg = ren.texture_from_file("bg.png");
     auto porky = ren.texture_from_file("porky.png");
+    double right = SCREEN_W - porky.width();
+    double bottom = SCREEN_H - porky.height();
 
-    int xtiles = SCREEN_W / TILE_SIZE + 1;
-    int ytiles = SCREEN_H / TILE_SIZE + 1;
+    vec2 position(right/2, bottom/2);
+    vec2 velocity = random(2.0, 4.0) * vec2(random(0.0, 2*PI));
 
-    ren.clear();
-    for (int y = 0; y < ytiles; ++y) {
-        for (int x = 0; x < xtiles; ++x) {
-            ren.copy(bg, x*TILE_SIZE, y*TILE_SIZE, TILE_SIZE, TILE_SIZE);
+    bool done = false;
+    SDL_Event e;
+    while (!done) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) {
+                done = true;
+            }
         }
+
+        // update position
+        position += velocity;
+
+        // In case of collision, reflect vector around normal
+        if (position.x < 0 || position.x > right ||
+            position.y < 0 || position.y > bottom)
+        {
+            auto N = normal((position.x < 0), (position.y < 0),
+                            (position.x > right), (position.y > bottom));
+            auto U = (velocity * N) * N;
+            auto W = velocity - U;
+            velocity = W - U;
+
+            // Apply random velocity and angle noise
+            velocity.length(random(2.0, 4.0));
+            velocity.angle(velocity.angle() + random(-0.7, 0.7));
+
+            // Clamp position to avoid jitter
+            position.x = std::max(0.0, std::min(right, position.x));
+            position.y = std::max(0.0, std::min(bottom, position.y));
+        }
+
+        render_scene(ren, bg, porky, position.x, position.y);
+        ren.present();
     }
-
-    ren.copy(porky, SCREEN_W/2 - 50, SCREEN_H/2 - 50);
-
-    ren.present();
-    
-    SDL_Delay(5000);
 
     return 0;
 }
