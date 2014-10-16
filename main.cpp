411d03f7a3e77106b0af778b4330143c2f83b171
Trip Volpe <trip.volpe@gmail.com>
2014-10-16 02:04:28 -0700

    Add sinewave background distortion effect

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory>
 #include <cmath>
 
 #include "sdl.h"
@@ -11,26 +12,6 @@ const int TILE_SIZE = 256;
 const double PI = 3.14159265;
 
 typedef sdl::renderer::texture texture;
-
-/*
- * Function: render_scene
- *
- * Render a tiled background with a sprite at a given location.
- */
-void render_scene(sdl::renderer& renderer,
-        texture const& bg, texture const& sprite, int x, int y)
-{
-    int xtiles = SCREEN_W / TILE_SIZE + 1;
-    int ytiles = SCREEN_H / TILE_SIZE + 1;
-
-    for (int ty = 0; ty < ytiles; ++ty) {
-        for (int tx = 0; tx < xtiles; ++tx) {
-            renderer.copy(bg, tx*TILE_SIZE, ty*TILE_SIZE, TILE_SIZE, TILE_SIZE);
-        }
-    }
-
-    renderer.copy(sprite, x, y);
-}
 
 
 /*
@@ -67,6 +48,113 @@ vec2 normal(bool left, bool top, bool right, bool bottom) {
 }
 
 
+/*
+ * Compute a specific frame of a distortion animation from
+ * a source surface and write it into a destination surface.
+ * (These should not be the same surface.)
+ *
+ * @param src   source surface
+ * @param dst   destination surface (must have same size and format)
+ * @param type  0=horz, 1=horz interlaced, 2=vert compression
+ * @param t     tick number
+ * @param A     amplitude of distortion wave
+ * @param F     frequency
+ * @param S     time scaling factor
+ * @param C     compression factor, for vertical effect
+ */
+int distort_frame(SDL_Surface* src, SDL_Surface* dst, int type, int t,
+        double A=16.0, double F=0.1, double S=0.1, double C=1.0)
+{
+    // Note that we're assuming 32-bit pixels here;
+    // this may change for other formats
+    int32_t* src_data = (int32_t*)src->pixels;
+    int32_t* dst_data = (int32_t*)dst->pixels;
+
+    for (int y = 0; y < src->h; ++y) {
+        int offset = A * sinf(F * y + S * t);
+
+        int new_x = 0;
+        int new_y = y;
+
+        if (type == 0) {
+            new_x = offset;
+        } else if (type == 1) {
+            new_x = (y % 2)? offset : -offset;
+        } else if (type == 2) {
+            new_y = (int)(y * C + offset + src->h) % src->h;
+        }
+
+        for (int x = 0; x < src->w; ++x) {
+            // Correctly wrap x offset
+            new_x = (new_x + src->w) % src->w;
+            dst_data[y*dst->w + x] = src_data[new_y*src->w + new_x];
+            ++new_x;
+        }
+    }
+    return 0;
+}
+
+
+/*
+ * Class: distortion
+ *
+ * A basic battle background supporting just one wave function.
+ * No palette transformations yet.
+ */
+class distortion
+{
+    sdl::surface src_;
+    std::unique_ptr<sdl::surface> dst_;
+
+    int type_;
+    double amplitude_;
+    double frequency_;
+    double timescale_;
+    double compression_;
+    int ticks_;
+
+public:
+    enum type {
+        horizontal,
+        interlaced,
+        compression
+    };
+
+    distortion(sdl::surface const& bg, int type,
+            double A, double F, double S, double C)
+        : src_(bg), type_(type), amplitude_(A),
+        frequency_(F), timescale_(S), compression_(C), ticks_(0)
+    {
+        // Allocate a destination surface the same size as source
+        // TODO: a copy method or somesuch, plus move semantics
+        SDL_Surface* copy = SDL_CreateRGBSurface(0,
+                src_.width(), src_.height(), 32,
+                0x000000ff,
+                0x0000ff00,
+                0x00ff0000,
+                0xff000000);
+        dst_.reset(new sdl::surface(copy));
+    }
+
+    void update() {
+        distort_frame(src_.handle(), dst_->handle(),
+              type_, ticks_, amplitude_, frequency_, timescale_,
+              compression_);
+        ++ticks_;
+    }
+
+    void render(sdl::renderer & renderer, int x, int y, int w, int h) {
+        // Do the stupid thing and create a texture right here
+        auto tex = renderer.texture_from_surface(*dst_);
+        renderer.copy(tex, x, y, w, h);
+    }
+
+    void render(sdl::renderer & renderer, int x, int y) {
+        render(renderer, x, y, src_.width(), src_.height());
+    }
+};
+
+
 int main(int argc, char** argv)
 {
     sdl::init(SDL_INIT_EVERYTHING);
@@ -83,13 +171,16 @@ int main(int argc, char** argv)
             SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
     // Load textures
-    auto bg = ren.texture_from_file("bg.png");
+    auto bg_surface = sdl::surface("bg.png");
     auto porky = ren.texture_from_file("porky.png");
     double right = SCREEN_W - porky.width();
     double bottom = SCREEN_H - porky.height();
 
     vec2 position(right/2, bottom/2);
     vec2 velocity = random(2.0, 4.0) * vec2(random(0.0, 2*PI));
+
+    // Create background distortion effect
+    distortion dist(bg_surface, 2, 16.0, 0.1, 0.1, 1.0);
 
     bool done = false;
     SDL_Event e;
@@ -99,6 +190,8 @@ int main(int argc, char** argv)
                 done = true;
             }
         }
+
+        dist.update();
 
         // update position
         position += velocity;
@@ -122,7 +215,9 @@ int main(int argc, char** argv)
             position.y = std::max(0.0, std::min(bottom, position.y));
         }
 
-        render_scene(ren, bg, porky, position.x, position.y);
+        dist.render(ren, 0, 0, SCREEN_W, SCREEN_H);
+        ren.copy(porky, position.x, position.y, porky.width()*2, porky.height()*2);
+
         ren.present();
     }
 
